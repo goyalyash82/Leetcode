8333606db925e8eff310d9176f4a79a020743148
goyalyash82 <goyalyash2310@gmail.com>
2025-07-22 23:49:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.63%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        int ct=0;
-      if(x==1) return 1;
-        for(int i=1;i<x;i++){
-            if(((1LL*i*i)<=x) &&(1LL*(i+1)*(i+1)>x)){
-                 ct=i;
-                 break;
+    int binarySqrt(int n) {
+        int s = 0;
+        int e = n;
+        long long int mid;
+        long long int ans = -1;
+
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            long long int square = mid * mid;
+
+            if (square == n)
+                return mid;
+
+            if (square < n) {
+                ans = mid;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
         }
-        return ct;
+        return ans;
+    }
+
+    int mySqrt(int x) {
+        return binarySqrt(x);
     }
 };
