e762d3d2aa2767002b1093a3f52b8e2be5011fd4
goyalyash82 <goyalyash2310@gmail.com>
2025-07-23 13:34:56 +0530

    Time: 6 ms (1.28%), Space: 12.4 MB (38.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=nums1.size()-1;i>=m;i--){
-            nums1.pop_back();
+        // for(int i=nums1.size()-1;i>=m;i--){
+        //     nums1.pop_back();
 
-        }
-        for(int i=nums2.size()-1;i>=n;i--){
-            nums2.pop_back();
+        // }
+        // for(int i=nums2.size()-1;i>=n;i--){
+        //     nums2.pop_back();
 
-        }
+        // }
+        nums1.resize(m);
+        nums2.resize(n);
                 nums1.insert(nums1.end(),nums2.begin(),nums2.end());
                 sort(nums1.begin(),nums1.end());
         cout<<"[";
